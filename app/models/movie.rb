ac78301b0b1c76288b0e78cd8c774a3ad7a861bf
Andre Menezes <djdedemenezes@gmail.com>
2022-04-06 15:21:33 -0300

    removed presence validation on release_date

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ApplicationRecord
 
   validates :title, :overview, presence: true
   validates :title, :poster_path, :backdrop_path, uniqueness: true
-  validates :release_date, presence: true, allow_blank: true, older: true
+  validates :release_date, allow_blank: true, older: true
   validates :poster_path, :backdrop_path, format: { with: /\.(jpg|gif|png)\z/i,
                                                     message: 'must be a URL for GIF, JPG or PNG image.' }
 end
