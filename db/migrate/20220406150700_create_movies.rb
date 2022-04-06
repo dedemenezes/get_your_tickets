5cd2365657bd8fd5ec0189f37cee8bc4b6629481
Andre Menezes <djdedemenezes@gmail.com>
2022-04-06 12:30:09 -0300

    ajusted migration

--- db/migrate/20220406150700_create_movies.rb
+++ db/migrate/20220406150700_create_movies.rb
@@ -2,8 +2,8 @@ class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
       t.string :title
-      t.string :overview
-      t.string :release_date
+      t.text :overview
+      t.date :release_date
       t.string :poster_path
       t.string :backdrop_path
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_143525) do
+ActiveRecord::Schema.define(version: 2022_04_06_150700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "overview"
+    t.date "release_date"
+    t.string "poster_path"
+    t.string "backdrop_path"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
