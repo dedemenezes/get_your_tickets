5e20bd4a6c249271fb2fab960f04c72a63ff37de
Andre Menezes <djdedemenezes@gmail.com>
2022-04-06 14:29:59 -0300

    validations and tests for it

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,19 @@
+class OlderValidator < ActiveModel::EachValidator
+  FIRST_MOVIE_DATE = Date.new(1888, 1, 1)
+
+  def validate_each(record, attribute, value)
+    value = '' if value.nil?
+    if value < FIRST_MOVIE_DATE
+      record.errors.add attribute, (options[:message] || "Can't be older than 'Roundhay Garden Scene'")
+    end
+  end
+end
+
 class Movie < ApplicationRecord
+  include ActiveModel::Validations
+
+  validates :title, :overview, presence: true
+  validates :release_date, presence: true, allow_blank: true, older: true
+  validates :poster_path, :backdrop_path, format: { with: /\.(jpg|gif|png)\z/i,
+                                                    message: 'must be a URL for GIF, JPG or PNG image.' }
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'movie title and overview must not be empty' do
+    movie = Movie.new
+    assert movie.invalid?
+    assert movie.errors[:title].any?
+    assert movie.errors[:overview].any?
+  end
+
+  test "movie must not be older than 'Roundhay Garden Scene'" do
+    movie = Movie.new(title: "aaa",
+                      overview: "very nice movie",
+                      release_date: Date.new(1887, 1, 1))
+    assert movie.invalid?
+    assert_equal ["Can't be older than 'Roundhay Garden Scene'"], movie.errors[:release_date]
+  end
+
+  def new_movie(image_path)
+    Movie.new(title: 'Harry',
+              overview: 'Potter',
+              release_date: Date.today,
+              backdrop_path: image_path,
+              poster_path: image_path)
+  end
+
+  test "movie images must be JPG, PNG or GIF" do
+    ok = %w[fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif]
+    bad = %w[fred.doc fred.gif/more fred.gif.more]
+
+    ok.each do |image_url|
+      assert new_movie(image_url).valid?, "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      subject = new_movie(image_url)
+      message = ['must be a URL for GIF, JPG or PNG image.']
+      assert subject.invalid?, "#{image_url} shouldn't be valid"
+      assert_equal message, subject.errors[:poster_path]
+      assert_equal message, subject.errors[:backdrop_path]
+    end
+  end
 end
