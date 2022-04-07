deafec8250edeb29e5ecadb8de410cc94471b237
Andre Menezes <djdedemenezes@gmail.com>
2022-04-07 15:21:04 -0300

    basic validation test and simple fixtures

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Room not valid wihtout capacity' do
+    subject = Room.new
+    assert subject.invalid?
+    assert subject.errors[:capacity].any?
+  end
+
+  test "Room capacity must not be bigger than 100" do
+    subject = rooms(:too_big)
+    assert subject.invalid?
+    assert subject.errors[:capacity].any?
+  end
 end
