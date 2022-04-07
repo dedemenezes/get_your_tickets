e48963eeb7ee77673583fb278965dee0b42849b4
Andre Menezes <djdedemenezes@gmail.com>
2022-04-07 15:57:31 -0300

    simple validators for exibithion

--- app/models/exibithion.rb
+++ app/models/exibithion.rb
@@ -1,4 +1,14 @@
 class Exibithion < ApplicationRecord
   belongs_to :room
   belongs_to :movie
+  validates :start_time, presence: true
+  validate :start_time_cannot_be_in_the_past
+
+  private
+
+  def start_time_cannot_be_in_the_past
+    if start_time.present? && start_time.past?
+      errors.add(:start_time, "The session can't be in the past")
+    end
+  end
 end
--- test/models/exibithion_test.rb
+++ test/models/exibithion_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ExibithionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Exibithion is not valid without start time' do
+    subject = Exibithion.new
+    assert subject.invalid?
+    assert subject.errors[:start_time].any?
+  end
+
+  test "start time can't be in the past" do
+    subject = Exibithion.new start_time: 24.hours.ago, room: rooms(:cinemark_one), movie: movies(:harry_potter)
+    assert subject.invalid?, "The session can't start in the past"
+    assert_equal ["The session can't be in the past"], subject.errors[:start_time]
+  end
 end
