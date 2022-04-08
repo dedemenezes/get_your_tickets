10888041afc282479a45ad42abcb54b54c384180
Andre Menezes <djdedemenezes@gmail.com>
2022-04-08 12:47:56 -0300

    can filter sessions by date

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,15 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @days_from_today = (DateTime.now..DateTime.now.days_since(6)).to_a
+
     @teathers = Teather.joins(:rooms, :exibithions).where(exibithions: { movie: @movie }).group(:id)
+    if params[:date]
+      @teathers = Teather.joins(:rooms, :exibithions)
+                         .where(exibithions: { movie: @movie,
+                                               start_time: params[:date].to_date.beginning_of_day..params[:date].to_date.end_of_day })
+                         .group(:id)
+    end
+
 
     fresh_when last_modified: @movie.updated_at.utc, etag: @movie
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
-  resources :movies, only: %i[index show]
+  resources :movies, only: %i[index show] do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
