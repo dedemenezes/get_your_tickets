e7ac3bf123465ef8ea86282486066f3c49ed198b
Andre Menezes <djdedemenezes@gmail.com>
2022-04-11 18:08:36 -0300

    route and controller to seats#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'movies#index'
-  resources :movies, only: %i[show]
+  resources :movies, only: %i[show] do
+    member do
+      resources :seats, only: %i[index]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
