3bf4b2bf0fd1f19368ab2a397c824c4cd9009c6f
Andre Menezes <djdedemenezes@gmail.com>
2022-04-11 18:00:25 -0300

    generated cart and lineitems models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_132527) do
+ActiveRecord::Schema.define(version: 2022_04_11_205852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "exibithions", force: :cascade do |t|
     t.datetime "start_time"
@@ -23,6 +28,15 @@ ActiveRecord::Schema.define(version: 2022_04_08_132527) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["movie_id"], name: "index_exibithions_on_movie_id"
     t.index ["room_id"], name: "index_exibithions_on_room_id"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "exibithion_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["exibithion_id"], name: "index_line_items_on_exibithion_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -76,6 +90,8 @@ ActiveRecord::Schema.define(version: 2022_04_08_132527) do
 
   add_foreign_key "exibithions", "movies"
   add_foreign_key "exibithions", "rooms"
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "exibithions"
   add_foreign_key "rooms", "teathers"
   add_foreign_key "seats", "rooms"
 end
---
+++ test/system/carts_test.rb
@@ -0,0 +1,10 @@
+require "application_system_test_case"
+
+class CartsTest < ApplicationSystemTestCase
+  # test 'must assign cart to new session' do
+  #   visit root_path
+  #   binding.pry
+
+  #   assert session[:cart_id], "Session must have cart assigned"
+  # end
+end
