eef5990d416a69da911c398ecf6d7cdf5fea48b0
Andre Menezes <djdedemenezes@gmail.com>
2022-04-11 23:25:05 -0300

    refctoring test on line item creating

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -12,27 +12,31 @@ class LineItemsController < ApplicationController
 
   def create
     @exibithion = Exibithion.find(params[:exibithion_id])
-    seat = Seat.find(choosen_seat_id.first)
-    @line_item = LineItem.new(
-      exibithion: @exibithion,
-      room: @exibithion.room,
-      seat: seat,
-      cart: @cart
-    )
-    seat.update(available: false)
+    @seat = Seat.find(choosen_seat_id.first)
+    @line_item = LineItem.new(accepted_params)
+    @seat.update(available: false)
     if @line_item.save
-      redirect_to movies_path
+      redirect_to cart_path(@cart)
     else
       render :new
     end
-
   end
 
   private
 
+  def accepted_params
+    {
+      exibithion: @exibithion,
+      room: @exibithion.room,
+      seat: @seat,
+      cart: @cart
+    }
+  end
+
   def choosen_seat_id
     params[:line_item][:seat_id].reject(&:blank?)
   end
+
   def line_item_params
     params.require(:line_item).permit(:seat_id)
   end
--- test/system/line_items_test.rb
+++ test/system/line_items_test.rb
@@ -1,21 +1,21 @@
 require "application_system_test_case"
 
 class LineItemsTest < ApplicationSystemTestCase
-  # test 'visit new from movie#show' do
-  #   visit movie_path(movies(:hp))
-  #   click_link("session_#{exibithions(:night_session).id}")
+  test 'visit new from movie#show' do
+    visit movie_path(movies(:hp))
+    click_link("session_#{exibithions(:night_session).id}")
 
-  #   assert_selector 'p', text: "Escolha seus assentos:"
-  # end
+    assert_selector 'p', text: "Escolha seus assentos:"
+  end
 
-  # test 'visiting the new' do
-  #   visit new_line_item_path(session_id: exibithions(:night_session).id)
+  test 'visiting the new' do
+    visit new_line_item_path(session_id: exibithions(:night_session).id)
 
-  #   # save_screenshot
-  #   assert_selector 'p', text: "Escolha seus assentos:"
-  #   assert_selector '.seats', count: 1
-  #   assert_selector '.seat', count: 2
-  # end
+    # save_screenshot
+    assert_selector 'p', text: "Escolha seus assentos:"
+    assert_selector '.seats', count: 1
+    assert_selector '.seat', count: 2
+  end
 
   test 'creating line_item' do
     assert_equal 3, LineItem.count
@@ -25,6 +25,6 @@ class LineItemsTest < ApplicationSystemTestCase
     click_button("Create Line item")
     assert_equal 4, LineItem.count
 
-    page.has_content?('Movies')
+    assert_current_path cart_path(LineItem.last.cart)
   end
 end
