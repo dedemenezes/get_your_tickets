afe491183b8794d39e814a0149358868b8c17e99
Andre Menezes <djdedemenezes@gmail.com>
2022-04-12 09:57:45 -0300

    cant book seat if another line item chose that seat

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,6 @@ class LineItem < ApplicationRecord
   belongs_to :exibithion
   belongs_to :room
   belongs_to :seat
+
+  validates :seat, uniqueness: { scope: :exibithion, message: "Seat is not available" }
 end
--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -4,4 +4,15 @@ class LineItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'seat must be available' do
+    subject = LineItem.new(
+      cart: carts(:two_line_items),
+      room: rooms(:cinemark_one),
+      seat: seats(:regular_available_seat_in_cinemark_one),
+      exibithion: exibithions(:night_session)
+    )
+    assert subject.invalid?, "Seat is not available"
+    assert_equal ["Seat is not available"], subject.errors[:seat]
+  end
 end
