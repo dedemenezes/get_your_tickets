b82d6b8c2207cffc6fb2e017b5af69404d95a5e9
Andre Menezes <djdedemenezes@gmail.com>
2022-04-12 12:33:05 -0300

    current cart at application controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,5 @@
 class CartsController < ApplicationController
-  include CurrentCart
-
   skip_before_action :authenticate_user!
-  before_action :set_cart
 
   def show
     @cart = Cart.find(params[:id]) unless @cart.id == params[:id]
