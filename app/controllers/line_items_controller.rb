a23bae39080d4a364cb79949d8ac7cc36e660f61
Andre Menezes <djdedemenezes@gmail.com>
2022-04-12 14:48:48 -0300

    refacoring line item creation

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -10,8 +10,12 @@ class LineItemsController < ApplicationController
 
   def create
     @exibithion = Exibithion.find(params[:exibithion_id])
-    @seat = Seat.find(choosen_seat_id.first)
-    @line_item = LineItem.new(accepted_params)
+    @line_item = LineItem.new
+    @line_item.exibithion = @exibithion
+    @line_item.room = @exibithion.room
+    @line_item.cart = @cart
+    @seat = Seat.find(line_item_params[:seat_id]).first
+    @line_item.seat = @seat
     if @line_item.save
       @seat.mark_as_occupied!
       redirect_to cart_path(@cart)
@@ -22,20 +26,7 @@ class LineItemsController < ApplicationController
 
   private
 
-  def accepted_params
-    {
-      exibithion: @exibithion,
-      room: @exibithion.room,
-      seat: @seat,
-      cart: @cart
-    }
-  end
-
-  def choosen_seat_id
-    params[:line_item][:seat_id].reject(&:blank?)
-  end
-
   def line_item_params
-    params.require(:line_item).permit(:seat_id)
+    params.require(:line_item).permit(seat_id: [])
   end
 end
